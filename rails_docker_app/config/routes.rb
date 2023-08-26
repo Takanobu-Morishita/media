2ac57d5ee571477a5a9420cc4eb56d38a2a2d4b0
Zukka <irei20030617@gmail.com>
2023-08-26 12:25:53 +0900

    made db

--- rails_docker_app/config/routes.rb
+++ rails_docker_app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'posts'     => 'posts#index'
 
   # Defines the root path route ("/")
   # root "articles#index"
--- rails_docker_app/db/seeds.rb
+++ rails_docker_app/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+#１つずつテストデータを追加する場合
+Post.create!(
+    title: "チーズ入りクッキー(6枚入り)",
+    body: "チーズクッキーは生地となるラングドシャだけでなく、間にサンドしたホワイトチョコにもチーズが入っています。ダイレクトにチーズの美味しさを感じられるのでチーズ好きにはたまらないクッキーです",
+    genre: "おかし",
+)
+
+100.times do |n|
+    Post.create!(
+        title: "test#{n + 1}人目", 
+        body: "test#{n + 1}回目",
+        genre: "test{n + 1}@test.com",
+)
+end
