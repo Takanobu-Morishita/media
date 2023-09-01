4021be3adaa75dc6c21d79e3b53817e28e40d434
Takanobu-Morishita <morishita@keio.jp>
2023-09-01 11:59:58 +0900

    usesテーブルにカラムを追加

--- rails_docker_app/db/schema.rb
+++ rails_docker_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_01_013934) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_01_025705) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -27,6 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_01_013934) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
