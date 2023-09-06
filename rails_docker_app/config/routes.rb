ff786ea6b4c2c8bcb39aba9bd74f4112cc3ffda0
haru <yui7162bayuus2013@gmail.com>
2023-09-06 18:46:43 +0900

    　編集画面作成

--- rails_docker_app/config/routes.rb
+++ rails_docker_app/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts, only:[:index, :show, :new, :create]
+  resources :posts, only:[:index, :show, :new, :create, :edit, :update]
   root to: "home#index"
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
