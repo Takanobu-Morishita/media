542c8e86472fe08f588fac63cc054617ef454439
red <yui7162bayuus2013@gmail.com>
2023-09-06 16:17:19 +0900

    userとpostを関連させる

--- rails_docker_app/db/schema.rb
+++ rails_docker_app/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_01_025705) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_05_151113) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.string "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
