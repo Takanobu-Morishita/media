538acaba4233832819ce6cc4236b5670e37a9642
Takanobu-Morishita <morishita@keio.jp>
2023-09-10 21:20:09 +0900

    ページネーションの実装

--- rails_docker_app/app/controllers/users_controller.rb
+++ rails_docker_app/app/controllers/users_controller.rb
@@ -1,38 +1,38 @@
 class UsersController < ApplicationController
-    def index
-        @users = User.all
-      end
-    
-      def show
-        @user = User.find(params[:id])
-      end
-      
-      def new
-        @user = User.new
-      end
-    
-      def create
-        @user = User.new(user_params)
-        @user.user_id = current_user.id
-        @user.save
-        redirect_to action: 'index'
-      end
-      
-      def edit
-        @user = User.find(params[:id])
-      end
-    
-      def update
-        @user = User.find(params[:id])
-        if @user.update(user_params)
-          redirect_to action: 'index'
-        else
-          render :new
-        end
-      end
-    
-      private
-      def user_params
-        params.require(:user).permit(:username, :profile, :email)
-      end
+  def index
+    @users = User.all.page(params[:page]).per(10)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.user_id = current_user.id
+    @user.save
+    redirect_to action: 'index'
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to action: 'index'
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :profile, :email)
+  end
 end
