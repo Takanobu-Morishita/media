788297ef1101cf5ef69b56a349f8633ea071bed3
Nastriat <tonic-colours-0j@icloud.com>
2023-09-17 16:18:56 +0900

    changed locale of notifications and alerts to japanese

--- rails_docker_app/app/controllers/users_controller.rb
+++ rails_docker_app/app/controllers/users_controller.rb
@@ -7,8 +7,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts
 
-    favorites = Favorite.where(user_id: current_user.id).pluck(:post_id)  # ログイン中のユーザーのお気に入りのpost_idカラムを取得
-    @favorite_list = Post.find(favorites)
+    if current_user.present?
+      favorites = Favorite.where(user_id: current_user.id).pluck(:post_id)  # ログイン中のユーザーのお気に入りのpost_idカラムを取得
+      @favorite_list = Post.find(favorites)
+    end
   end
   
   def new
--- rails_docker_app/db/schema.rb
+++ rails_docker_app/db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_16_235314) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -39,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_16_235314) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.text "profile"
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
