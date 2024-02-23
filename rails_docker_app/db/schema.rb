04178ad9ee12c55b879d263988dd339247aac840
Takanobu-Morishita <morishita@keio.jp>
2024-02-23 21:15:44 +0900

    フォロ機能の実装

--- rails_docker_app/db/schema.rb
+++ rails_docker_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_18_125351) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_23_114905) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -67,6 +67,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_18_125351) do
     t.string "url_title"
     t.string "url"
     t.string "image"
+  end
+
+  create_table "relationships", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
